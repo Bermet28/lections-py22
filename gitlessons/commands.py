e12a22a654653f29cf2ec3e124ad834fd3e7600a
Bermet28 <bemochka29@gmail.com>
2022-07-13 13:21:46 +0600

    4 and 5 commands

--- gitlessons/commands.py
+++ gitlessons/commands.py
@@ -1,1 +1,8 @@
-a = "knvtyitgyuhk"
+git remote add -это команда для того чтобы связать 
+
+git remote add название подключения <
+git remote add origin <url>
+git push - после коммита изменений при помощи этой команды мы отправляем наши изменения в ыайлах на удаленный репозиторий 
+git push origin> <название ветки>(main)
+git push origin main
+ kkkkkkkkkk
