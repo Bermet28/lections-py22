d8b03ad6f77808f3dcdbb3f3c595226a7ea1bd79
Bermet28 <bemochka29@gmail.com>
2022-07-13 13:38:10 +0600

    comment

--- gitlessons/commands.py
+++ gitlessons/commands.py
@@ -5,4 +5,15 @@ git remote add origin <url>
 git push - после коммита изменений при помощи этой команды мы отправляем наши изменения в ыайлах на удаленный репозиторий 
 git push origin> <название ветки>(main)
 git push origin main
- kkkkkkkkkk
+ ----------------------------------------------------
+ 1. git init 
+ 2. git branch -M main (переименовываем гл ветку с master на main)
+ 3. git add .
+ 4. git commit -m 'comment' (все добавлено в локальный депо)
+ 5. git remote add origin  <url>
+ 6. git push origin main
+ ////////////////////////
+ 
+ git add .
+ git commit -m 'comment'
+ git push origin main
